9997ad5faebd2cd07f137a97a29b9024ae9437d2
Tanlyn78 <>
2025-11-14 18:33:16 +0000

    User-defined functions

---
+++ functions/user_defined_functions/main.py
@@ -0,0 +1,19 @@
+def calculate_total_cost(price,quantity):
+    total_cost = price * quantity
+    return total_cost
+
+# Call the function and print the result
+apples_total_cost = calculate_total_cost(1.50, 10)
+
+print(f"The total cost for apples is ${apples_total_cost}")
+
+
+
+#def function_name(argument_1, argument_2):
+   # Code block
+ #  return result
+
+#def greet_customer(name):
+  #  print(f"Hello, {name}! Welcome to our store.")
+
+#greet_customer("Alice")
